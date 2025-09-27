45b4f11e478c15556bcc23c4e6ff8af65a9199a4
mohammad-sameer-shishani <mohammad.s.shishani@gmail.com>
2025-09-28 00:01:47 +0300

    Initialize vehicle lookup UI application with core components and routing
    - Created app component with title signal and routing setup.
    - Added unit tests for app component.
    - Implemented error handling components for Not Found and Server Error pages.
    - Configured environment settings for development and production.
    - Established global styles and theme options using Tailwind CSS and DaisyUI.
    - Set up TypeScript configuration for the application and testing.
    - Created index.html as the entry point for the application.

---
+++ vehicle-lookup-ui/src/shared/errors/server-error/server-error.ts
@@ -0,0 +1,24 @@
+import { Component, inject, signal, Signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiError } from '../../../types/error';
+
+@Component({
+  selector: 'app-server-error',
+  imports: [],
+  templateUrl: './server-error.html',
+  styleUrl: './server-error.css'
+})
+export class ServerError {
+  protected error:ApiError;
+  private router=inject(Router);
+  protected showDetails=false;
+  detailsToggle(){
+    this.showDetails=!this.showDetails;
+  }
+
+  constructor() {
+    const navigation=this.router.getCurrentNavigation();
+    this.error=navigation?.extras?.state?.['error'];
+   }
+ 
+}
